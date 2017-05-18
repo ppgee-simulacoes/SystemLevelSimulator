289ee742ce13c0ee8a4f904d7a4a641d8ad2801d
CalilQ <calil_queiroz@hotmail.com>
2017-05-18 12:08:21 -0300

    Create Simulation Thread class skeleton

---
+++ src/simulation_thread.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 11:04:05 2017
+
+@author: Calil
+"""
+
+class SimulationThread(object):
+    """
+        Performs simulation thread, with multiple drops.
+        
+        Properties:
+            param <Parameters>: simulation parameters
+            topology <Topology>: network toloplogy and BS positions
+            bs_list <list>: list of all the BS objects
+            
+        Constructor:
+            Syntax: self = SimulationThread(param)
+            Inputs: param <Parameters>: simulation parameters
+            
+        Methods:
+            create_bs:
+                Uses topology to create BS objects and save them in bs_list
+                property.
+                
+        Author: Calil Queiroz
+                calil_queiroz@hotmail.com
+                
+        Version History:
+            V. 0.1 (May 18 2017) - create class skeleton  
+    """
+    
+    def __init__(self,param):
+        pass
+    
+    def create_bs(self):
+        pass
+    
