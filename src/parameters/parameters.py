4f59d8798b4f3dad8787f0e47995dd6961755512
CalilQ <calil_queiroz@hotmail.com>
2017-05-18 12:07:07 -0300

    Add first properties to Parameters class

--- src/parameters/parameters.py
+++ src/parameters/parameters.py
@@ -10,7 +10,9 @@ class Parameters(object):
         Parameters class, which holds the simulation parameters.
         
         Properties:
-            
+            cell_radius <1x1 float>: radius of cell [meters]
+            num_layers <1x1 int>: number of interference layers
+            bs_height <1x1 float>: height of base stations [meters]
         
         Constructor:
             Syntax: self = Parameters()
@@ -33,5 +35,14 @@ class Parameters(object):
             Parameters.__instance = object.__new__(cls)
         return Parameters.__instance
     
-    #########################################################################
+    ###########################################################################
     # SIMULATION PARAMETERS
+    
+    # Radius of cell [meters]
+    cell_radius = 200
+    
+    # Number of interference layers
+    num_layers = 1
+    
+    # Height of Base Stations [meters]
+    bs_height = 10
