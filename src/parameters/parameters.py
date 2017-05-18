210d2a580cac6b8168e8cd7e3ca54e70bfb1011e
CalilQ <calil_queiroz@hotmail.com>
2017-05-18 11:18:11 -0300

    Add header to Parameters class

--- src/parameters/parameters.py
+++ src/parameters/parameters.py
@@ -1,20 +1,40 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu May 18 10:42:36 2017
-
-@author: Calil
-"""
+# Parameters Class:
+# 1) Holds simulation parameters
+# 2) Properties:
+#    
+# 3) Constructor:
+#    Syntax: self = Parameters()
+# 4) Author(s):
+#    Calil Queiroz;
+#    calil_queiroz@hotmail.com;
+# 6) Version History:
+#    V. 0.1 (May 18 2017) - create class skeleton
 
 class Parameters(object):
+    """
+        Parameters class, which holds the simulation parameters.
+        
+        Properties:
+            
+        
+        Constructor:
+            Syntax: self = Parameters()
+            
+        Author: Calil Queiroz
+                calil_queiroz@hotmail.com
+                
+        Version History:
+            V. 0.1 (May 18 2017) - create class skeleton
+    """
     
     __instance = None
-    
         
     def __new__(cls):
-        """
-        This is the Singleton Pattern to ensure that this class will have only
-        one instance
-        """
+
+        #This is the Singleton Pattern to ensure that this class will have only
+        #one instance
+
         if Parameters.__instance is None:
             Parameters.__instance = object.__new__(cls)
         return Parameters.__instance
