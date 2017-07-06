Fixed bug in UL Interference calculation

src/base_station.py
@@ -55,7 +55,8 @@ class BaseStation(object):
 
         self.__ms_rx_power = []
 
-        self.__interference_power = None
+        self.__interference_power = []
+        self.__selected_interference_power = None
         self.__connected_ms_list = []
         
     def connect_to(self, ms):
@@ -95,17 +96,24 @@ class BaseStation(object):
 
         return antenna_gain
 
-    def calculate_snir(self, random_state):
+    def calculate_statistics(self, random_state):
 
         # Get random active MS from list of connected MSs and choose its power as current received power
         active_ms_index = random_state.randint(0, high=len(self.connected_ms_list))
-        rx_power = self.ms_rx_power[active_ms_index]
+        rx_power = 10 ** (self.ms_rx_power[active_ms_index] / 10)
 
-        interference = 10 ** (np.asarray(self.interference_power) / 10)
+        self.selected_interference_power = np.delete(self.selected_interference_power,
+                                                     np.where(self.selected_interference_power == 0))
+
+        interference = 10 ** (self.selected_interference_power / 10)
         noise = 10 ** (self.noise / 10)
-        snir = rx_power - 10 * (np.log10(sum(interference) + noise))
 
-        return snir
+        snir = rx_power / sum(interference) + noise
+        snir_db = 10 * (np.log10(snir))
+
+        throughput = self.tx_band * np.log2(1 + snir)
+
+        return snir_db, throughput
 
     def reset_list(self):
         self.__connected_ms_list = []
@@ -147,9 +155,13 @@ class BaseStation(object):
     def interference_power(self):
         return self.__interference_power
 
-    @interference_power.setter
-    def interference_power(self, interference_power):
-        self.__interference_power = interference_power
+    @property
+    def selected_interference_power(self):
+        return self.__selected_interference_power
+
+    @selected_interference_power.setter
+    def selected_interference_power(self, selected_interference_power):
+        self.__selected_interference_power = selected_interference_power
 
     @property
     def noise(self):

src/mobile_station.py
@@ -51,12 +51,18 @@ class MobileStation(object):
     def connect_to(self, bs):
         self.__connected_bs = bs
 
-    def calculate_snir(self):
+    def calculate_statistics(self):
+
         interference = 10**(np.asarray(self.interference_power)/10)
         noise = 10**(self.noise/10)
-        snir = self.rx_power - 10*(np.log10(sum(interference) + noise))
+        rx_power = 10**(self.rx_power/10)
 
-        return snir
+        snir = rx_power / (sum(interference) + noise)
+        snir_db = 10 * np.log10(snir)
+
+        throughput = self.tx_band * np.log2(1 + snir)
+
+        return snir_db, throughput
 
     @property
     def index(self):

src/results.py
@@ -12,39 +12,66 @@ class Results(object):
 
         self.__snir_DL = []
         self.__snir_UL = []
+        self.__throughput_DL = []
+        self.__throughput_UL = []
 
-    def add_snir(self, snir_vector_DL, snir_vector_UL):
-        self.__snir_DL.append(snir_vector_DL)
-        self.__snir_UL.append(snir_vector_UL)
+    def add_statistics(self, statistics_DL, statistics_UL):
 
-    def plot_snir_cdf(self):
-        ax_DL, val_DL = self._plot_cdf(self.__snir_DL)
-        ax_UL, val_UL = self._plot_cdf(self.__snir_UL)
+        snir_vector_DL, throughput_DL = statistics_DL
+        snir_vector_UL, throughput_UL = statistics_UL
 
-        ax_DL.set_xlabel("SNIR [dB]")
-        ax_DL.set_ylabel("CDF of SNIR")
-        ax_DL.set_title("SNIR for the Downlink")
-        ax_DL.set_xlim([np.min(val_DL), np.max(val_DL)])
-        ax_DL.set_ylim([0, 1])
-        ax_DL.xaxis.grid(True)
-        ax_DL.yaxis.grid(True)
+        self.snir_DL.append(snir_vector_DL)
+        self.snir_UL.append(snir_vector_UL)
+        self.throughput_DL.append(throughput_DL)
+        self.throughput_UL.append(throughput_UL)
 
-        ax_UL.set_xlabel("SNIR [dB]")
-        ax_UL.set_ylabel("CDF of SNIR")
-        ax_UL.set_title("SNIR for the Uplink")
-        ax_UL.set_xlim([np.min(val_UL), np.max(val_UL)])
-        ax_UL.set_ylim([0, 1])
-        ax_UL.xaxis.grid(True)
-        ax_UL.yaxis.grid(True)
+    def plot_statistics_cdf(self):
 
-        return ax_DL, ax_UL
+        ax_snir_DL, snir_DL = self._plot_cdf(self.snir_DL)
+        ax_snir_UL, snir_UL = self._plot_cdf(self.snir_UL)
+        ax_tput_DL, tpt_DL = self._plot_cdf(self.throughput_DL)
+        ax_tput_UL, tpt_UL = self._plot_cdf(self.throughput_UL)
+
+        ax_snir_DL.set_xlabel("SNIR [dB]")
+        ax_snir_DL.set_ylabel("CDF of SNIR")
+        ax_snir_DL.set_title("SNIR for the Downlink")
+        ax_snir_DL.set_xlim([np.min(snir_DL), np.max(snir_DL)])
+        ax_snir_DL.set_ylim([0, 1])
+        ax_snir_DL.xaxis.grid(True)
+        ax_snir_DL.yaxis.grid(True)
+
+        ax_snir_UL.set_xlabel("SNIR [dB]")
+        ax_snir_UL.set_ylabel("CDF of SNIR")
+        ax_snir_UL.set_title("SNIR for the Uplink")
+        ax_snir_UL.set_xlim([np.min(snir_UL), np.max(snir_UL)])
+        ax_snir_UL.set_ylim([0, 1])
+        ax_snir_UL.xaxis.grid(True)
+        ax_snir_UL.yaxis.grid(True)
+
+        ax_tput_DL.set_xlabel("Throughput [bps]")
+        ax_tput_DL.set_ylabel("CDF of Throughput")
+        ax_tput_DL.set_title("Throughput for the Downlink")
+        ax_tput_DL.set_xlim([np.min(tpt_DL), np.max(tpt_DL)])
+        ax_tput_DL.set_ylim([0, 1])
+        ax_tput_DL.xaxis.grid(True)
+        ax_tput_DL.yaxis.grid(True)
+
+        ax_tput_UL.set_xlabel("Throughput [bps]")
+        ax_tput_UL.set_ylabel("CDF of Throughput")
+        ax_tput_UL.set_title("Throughput for the Uplink")
+        ax_tput_UL.set_xlim([np.min(tpt_UL), np.max(tpt_UL)])
+        ax_tput_UL.set_ylim([0, 1])
+        ax_tput_UL.xaxis.grid(True)
+        ax_tput_UL.yaxis.grid(True)
+
+        return ax_tput_UL, ax_snir_UL, ax_tput_DL, ax_snir_DL
 
     def _plot_cdf(self, values_list):
         values = np.hstack(np.array(values_list))
         values = np.sort(values)
         prob_values = np.arange(0, len(values))/len(values)
 
-        fig = plt.figure(figsize=(6,6))
+        fig = plt.figure(figsize=(6, 6))
         ax = fig.add_subplot(111)
 
         ax.plot(values, prob_values)
@@ -54,6 +81,8 @@ class Results(object):
     def reset(self):
         self.__snir_DL = []
         self.__snir_UL = []
+        self.__throughput_DL = []
+        self.__throughput_UL = []
 
     @property
     def snir_DL(self):
@@ -62,3 +91,11 @@ class Results(object):
     @property
     def snir_UL(self):
         return self.__snir_UL
+
+    @property
+    def throughput_DL(self):
+        return self.__throughput_DL
+
+    @property
+    def throughput_UL(self):
+        return self.__throughput_UL

src/simulation_thread.py
@@ -121,9 +121,13 @@ class SimulationThread(object):
         # Perform Loop
         self.run_loop()
         # Save results in future releases
-        snir_dl, snir_up = self.results.plot_snir_cdf()
+        throughput_ul, snir_ul, \
+        throughput_dl, snir_dl = self.results.plot_statistics_cdf()
+
+        plt.show(throughput_dl)
         plt.show(snir_dl)
-        plt.show(snir_up)
+        plt.show(throughput_ul)
+        plt.show(snir_ul)
 
     def run_loop(self):
 
@@ -138,9 +142,9 @@ class SimulationThread(object):
                 # Connects the MSs to each BSs according to the received power
                 self.connect_ms_to_bs()
                 # Get SNIR Vector from current drop
-                snir_vector_DL, snir_vector_UL = self.get_snir()
+                statistics_DL, statistics_UL = self.get_statistics()
                 # Save statistics from current drop
-                self.results.add_snir(snir_vector_DL, snir_vector_UL)
+                self.results.add_statistics(statistics_DL, statistics_UL)
                 # Plots the Simulation Grid
                 if self.parameters.plot_drop_grid:
                     grid = self.plot_grid()
@@ -190,7 +194,10 @@ class SimulationThread(object):
                 distance = np.sqrt(relative_position[0] ** 2 + relative_position[1] ** 2)
 
                 # Calculate antenna gains
-                antenna_gain = bs.calculate_antenna_gain(ms.position)
+                if self.parameters.sectorization:
+                    antenna_gain = bs.calculate_antenna_gain(ms.position)
+                else:
+                    antenna_gain = 0
 
                 # Calculate path loss and received power for different propagation models
                 path_loss = self.path_loss.calculate_path_loss(distance)
@@ -200,6 +207,8 @@ class SimulationThread(object):
                 bs_rx_power = ms.tx_power - path_loss + antenna_gain
 
                 ms.interference_power.append(ms_rx_power)
+
+                bs.interference_power.append(bs_rx_power)
 
                 # Choose BS with maximum received power
                 if ms_rx_power > power_max:
@@ -220,41 +229,49 @@ class SimulationThread(object):
 
         self.__connected = True
 
-    def get_bs_interference(self, bs_band):
+    def get_bs_interference(self, current_bs):
 
         # Create vector of interference power from each BS
         interference_power_vector = np.zeros(len(self.bs_list))
         for bs in self.bs_list:
             num_connected_ms = len(bs.connected_ms_list)
             # Interference occurs only if there are MSs connected to the BS and it is not using the same band
-            if num_connected_ms > 0 and bs.tx_band_index != bs_band:
+            if num_connected_ms > 0 and bs.tx_band_index == current_bs.tx_band_index:
                 # Choose random connected MS as the active at interference level
                 active_ms_index = self.random_states[RandomSeeds.ACTIVE_MOBILE.value].\
                     randint(0, high=num_connected_ms)
-                active_ms_power = bs.ms_rx_power[active_ms_index]
+                active_ms = bs.connected_ms_list[active_ms_index]
+                active_ms_power = current_bs.interference_power[active_ms.index]
                 interference_power_vector[bs.index] = active_ms_power
 
         return interference_power_vector
 
-    def get_snir(self):
+    def get_statistics(self):
 
         snir_vector_DL = np.zeros(self.__num_ms)
+        throughput_DL = np.copy(snir_vector_DL)
         snir_vector_UL = np.zeros(len(self.bs_list))
+        throughput_UL = np.copy(snir_vector_UL)
 
         for ms in self.ms_list:
             # Get SNIR for each MS
-            snir_vector_DL[ms.index] = ms.calculate_snir()
+            snir_vector_DL[ms.index], throughput_DL[ms.index] = ms.calculate_statistics()
 
         for bs in self.bs_list:
             # Get vector of interference power and delete element corresponding to current BS
-            bs.interference_power = np.delete(self.get_bs_interference(bs.tx_band_index), bs.index)
+            bs.selected_interference_power = np.delete(self.get_bs_interference(bs), bs.index)
             if bs.connected_ms_list:
-                snir_vector_UL[bs.index] = bs.calculate_snir(self.random_states[RandomSeeds.ACTIVE_MOBILE.value])
+                snir_vector_UL[bs.index], \
+                throughput_UL[bs.index] = bs.calculate_statistics(self.random_states[RandomSeeds.ACTIVE_MOBILE.value])
 
         # Eliminate BSs that are not causing interference
         snir_vector_UL = np.delete(snir_vector_UL, np.where(snir_vector_UL == 0))
+        throughput_UL = np.delete(throughput_UL, np.where(throughput_UL == 0))
 
-        return snir_vector_DL, snir_vector_UL
+        statistics_DL = snir_vector_DL, throughput_DL
+        statistics_UL = snir_vector_UL, throughput_UL
+
+        return statistics_DL, statistics_UL
 
     def plot_grid(self):
 